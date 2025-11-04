17368652d3785130a526ecd65d1eaee38165019d
Bazzz666 <solceller.syd@icloud.com>
2025-11-04 01:28:38 -0800

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { calculateBatteryROI, ROIInputs, ROIOutputs } from "./lib/roi-calculator";
+export { runSanityCheck, SanityResult } from "./lib/roi-sanity";
+export { generateBatteryQuotePdf } from "./lib/pdf-generator";
