36bf70b8623e0d399cc6b613da7e095b610f6773
Bazzz666 <solceller.syd@icloud.com>
2025-11-06 05:50:05 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { calculateBatteryROI, ROIInputs, ROIOutputs } from "./lib/roi-calculator";
 export { runSanityCheck, SanityResult } from "./lib/roi-sanity";
 export { generateBatteryQuotePdf } from "./lib/pdf-generator";
+
+export { generateEnhancedQuotePdf } from "./lib/pdf-print";
